f7e0152d9d536aec3e2ad58f592d18ac39e4ef9a
vipulsharma-neenopal <vipul.sharma@neenopal.com>
2026-05-15 13:53:19 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const port = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
-  res.end('Hello from Azure CI/CD Demo!');
+  res.end('Hello from Azure CI/CD Pipeline!');
 });
 
 server.listen(port, hostname, () => {
